2f479307bb91ec7b38495868763e37c5503b2eb0
maggie-lin <maggie_lin@comcast.com>
2016-10-03 17:41:55 -0700

    starting replica of DART Ad Units page: general divs in place + adding formatting and color

---
+++ DARTAdUnits.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DART Ad Units Replica</title>
+    <link rel="stylesheet" type="text/css" href="DARTAdUnits.css">
+  </head>
+  <body>
+    <div id="whole">
+      <div id="mainbanner">
+        <div id="mainleft">
+          <img src="/maggie/visualizeMPS/logo.png">
+        </div>
+        <div id="mainright">
+          <div id="mainrighttop">
+          </div>
+          <div id="mainrightbottom">
+            <ul>
+              <li><a href="google.com">Pages</a></li>
+              <li><a href="google.com">Sections</a></li>
+              <li><a href="google.com">Content Types</a></li>
+              <li><a href="google.com">Packages</a></li>
+              <li><a class="active" href="google.com">Globals</a></li>
+              <li><a href="google.com">Scheduling</a></li>
+              <li><a href="google.com">Logs & Tools</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div id="secondbanner">
+
+      </div>
+      <table id="adunits">
+        <caption id="main">4 Dart Ad Units</caption></nobr>
+        <caption id="second">[page 1 of 1]</caption>
+        <tr>
+          <th>Weight</th>
+          <th>Unit Name</th>
+          <th>GPT Slot</th>
+          <th>GPT Ad Id Override</th>
+          <th>Sizes</th>
+          <th>Responsive Map</th>
+          <th>Detected Display</th>
+          <th>Mobile</th>
+          <th>Collapse</th>
+          <th>Unit Append</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+      <button id="addDARTAdUnit"></button>
+      <div id="bottomtext"></div>
+      <div id="footer"></div>
+    </div>
+
+  </body>
+</html>
